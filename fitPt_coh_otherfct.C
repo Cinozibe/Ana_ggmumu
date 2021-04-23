61c2376a98b7c283eedc4fca741681f916daba5a
Nicolas Bizé <nicolasbize@services-mbp.home>
2021-04-23 09:31:43 +0200

    add a different fit on coherent with an other function (log normal + tail)

--- fitPt_coh_otherfct.C
+++ fitPt_coh_otherfct.C
@@ -53,16 +53,16 @@ Double_t TailExpo(Double_t *x, Double_t *par)
 
 Double_t sum(Double_t *x, Double_t *par)
 {
-    return PowerLaw(x,par) + Gaussian(x, &par[4]) + Gaussian(x, &par[7]);
+    return LogNormal(x,par) + TailExpo(x, &par[3]);
 }
 
 /*
 Double_t sum(Double_t *x, Double_t *par)
 {
-    return PowerLaw(x,par) + Gaussian(x, &par[4]) + TailExpo(x, &par[7]);
+    return PowerLaw(x,par) + TailExpo(x, &par[4]);
 }
 */
-void fitPt_coh()
+void fitPt_coh_otherfct()
 {
  
  
@@ -76,7 +76,7 @@ void fitPt_coh()
  fHistoDiMuonOS->GetAxis(0)->SetRange(fHistoDiMuonOS->GetAxis(0)->FindBin(1.9),fHistoDiMuonOS->GetAxis(0)->FindBin(2.6));
  fHistoDiMuonOS->GetAxis(1)->SetRange(fHistoDiMuonOS->GetAxis(1)->FindBin(0.),fHistoDiMuonOS->GetAxis(1)->FindBin(1.));
  
- Double_t par[10];
+ Double_t par[6];
  
  TF1* PowerLawfct = new TF1("PowerLawfct", PowerLaw, 0.,1.,4);
  PowerLawfct->SetParameters(1.,1.,1.,1.);
@@ -86,7 +86,7 @@ void fitPt_coh()
  TF1* LogNormalfct = new TF1("LogNormalfct", LogNormal, 0.,1.,3);
  LogNormalfct->SetParameters(1.,1.,1.);
  LogNormalfct->SetLineStyle(4);
- LogNormalfct->SetLineColor(kGreen);
+ LogNormalfct->SetLineColor(kRed);
 
  TF1* Gaussianfct = new TF1("Gaussianfct", Gaussian, 0.,1.,3);
  Gaussianfct->SetParameters(1.,1.,1.);
@@ -104,10 +104,10 @@ void fitPt_coh()
  Tail->SetLineStyle(4);
 
 
- TF1* GlobalFit = new TF1("GlobalFit", sum, 0.,1.,10);
- GlobalFit->SetParameters(1.,1.,1.,1. ,1.,1.,1., 1.,1.,1.);
+ TF1* GlobalFit = new TF1("GlobalFit", sum, 0.,1.,5);
+ GlobalFit->SetParameters(1.,1.,1. ,1.,1.);
  //GlobalFit->SetParLimits(0,1.,500.);
- GlobalFit->SetParLimits(2,0.,10.);
+ //GlobalFit->SetParLimits(2,0.,10.);
  //GlobalFit->SetParLimits(7,0.,1.e7);
  //GlobalFit->SetParLimits(5,0.,2.);
  //GlobalFit->SetParLimits(8,0.,2.);
@@ -127,7 +127,7 @@ void fitPt_coh()
 
  fHistoDiMuonOS->GetAxis(0)->SetRange(fHistoDiMuonOS->GetAxis(0)->FindBin(3.4),fHistoDiMuonOS->GetAxis(0)->FindBin(5.));
  TH1D* h3 = fHistoDiMuonOS->Projection(1);
- h3->SetTitle("Fit PowerLaw + Gaus + Gaus - Pt Distribution - mass range [3.4 - 5] - coh; p_{T} GeV/c; # DiMuons");
+ h3->SetTitle("Fit Log Normal + TailExpo - Pt Distribution - mass range [3.4 - 5] - coh; p_{T} GeV/c; # DiMuons");
  //GlobalFit->SetParameters(1.,1.,1.,1.,1.,1.,1.);
  //h3->Fit("GlobalFit","R","");
 
@@ -138,44 +138,24 @@ void fitPt_coh()
  //GlobalFit->SetParameters(1.,1.,1.,1.,1.,1.,1.);
  
 
- TCanvas* c1 = new TCanvas("FitPowerLawTwoGaus_Pt Distribution_1.9_5._coh","mass fct pt coh");
+ TCanvas* c1 = new TCanvas("FitLogNormalTailExpo_Pt Distribution_3.4_5._coh","mass fct pt coh");
  c1->cd();
  gPad->SetLogy();
  gStyle->SetOptFit(1111);
  h3->Draw("e");
  h3->Fit("GlobalFit","EM","",0.,1.);
  
- const char *formula = "f( p_{T} ) = p_{T} p0 #left( #frac{ 1 }{ #left(1 + #frac{ p_{T} }{ p1 } #right)^{p2} } #right)^{p3}+ p_{T} p4 exp#left(- #frac{#left(p_{T} - p5#right)^{2}}{2 p6^{2}}#right) + p7p_{T} exp#left(- #frac{#left(p_{T} - p8#right)^{2}}{2 p9^{2}}#right)";
+ const char *formula = "f( p_{T} ) = #frac{p0}{2#pi p2 p_{T}} exp#left( - #frac{ln^{2}#left(p_{T} - p1#right)}{2 p2^{2}} #right)+ p3 p_{T} exp#left(- #frac{p_{T}}{p4}#right)";
  TLatex latex;
  latex.SetTextSize(0.025);
  latex.DrawLatex(.1,100.,formula);
  
  GlobalFit->GetParameters(par);
- PowerLawfct->SetParameters(par);
- Gaussianfct->SetParameters(&par[4]);
- Gaussianfct2->SetParameters(&par[7]);
- PowerLawfct->Draw("same");
- Gaussianfct->Draw("same");
- Gaussianfct2->Draw("same");
+ LogNormalfct->SetParameters(par);
+ //Gaussianfct->SetParameters(&par[3]);
+ Tail->SetParameters(&par[3]);
+ LogNormalfct->Draw("same");
+ //Gaussianfct->Draw("same");
+ Tail->Draw("same");
 
-
-/*
- 
- c1->cd(2);gPad->SetLogy();
- h2->Draw("e");
- c1->cd(3);gPad->SetLogy();
- h3->Draw("e");
- c1->cd(4);gPad->SetLogy();
- h4->Draw("e");
-
- TFile *fout = TFile::Open("FittedLogN_coh.root", "RECREATE");
- fout->cd(); 
- h1->Write("PtDistribution Mass 19 26 coh");
- h2->Write("PtDistribution Mass 26 34 coh");
- h3->Write("PtDistribution Mass 34 50 coh");
- h4->Write("PtDistribution Mass 19 50 coh");
- c1->Write("PtDistribution coh");
- 
- delete fout;
- */
 }

2fedc4962c9a2d671db4380aaa687b2fdf4ff57d
Nicolas Bizé <nicolasbize@services-mbp.home>
2021-04-23 09:07:05 +0200

    add macro fit on data mass distribution and differents fits on coh MC

---
+++ fitPt_coh_otherfct.C
@@ -0,0 +1,161 @@
+#include <iostream>
+#include <string>
+#include "TFile.h"
+#include "TList.h"
+#include "THnSparse.h"
+#include "TH1D.h"
+#include "TCanvas.h"
+#include "TChain.h"
+#include "TLatex.h"
+#include "TF1.h"
+#include "TStyle.h"
+#include "TF1Convolution.h"
+//#include "FitFunctions.C"
+#include <cmath>
+#include <vector>
+#include <stdio.h>
+#include "TMath.h"
+#include "TObjArray.h"
+
+Double_t PowerLaw(Double_t *x, Double_t *par)
+{
+    return x[0] * par[0] * TMath::Power( 1 / TMath::Power(1+x[0]/par[1],par[2]) ,par[3]);
+}
+
+
+Double_t LogNormal(Double_t *x, Double_t *par)
+{
+    //par[1]=mean
+    //par[2]=width
+    //par[0]=normalization
+
+    Double_t numExp =  (TMath::Log(x[0]) - par[1]) * (TMath::Log(x[0]) - par[1])  ;
+    Double_t denExp = 2 * par[2] * par[2];
+    
+    return par[0]/(x[0] * par[2] * TMath::Sqrt(2*TMath::Pi())) * TMath::Exp(- numExp / denExp) ;
+    
+}
+
+Double_t Gaussian(Double_t *x, Double_t *par)
+{
+    //par[0]=normalization
+    //par[1]=mean
+    //par[2]=width
+    
+    return x[0] * par[0] * TMath::Exp( -0.5 * (x[0]-par[1]) * (x[0]-par[1]) / (par[2] * par[2]) );
+}
+
+Double_t TailExpo(Double_t *x, Double_t *par)
+{
+    return x[0] * par[0] * TMath::Exp( - x[0] / par[1]);
+}
+
+
+Double_t sum(Double_t *x, Double_t *par)
+{
+    return LogNormal(x,par) + TailExpo(x, &par[3]);
+}
+
+/*
+Double_t sum(Double_t *x, Double_t *par)
+{
+    return PowerLaw(x,par) + TailExpo(x, &par[4]);
+}
+*/
+void fitPt_coh_otherfct()
+{
+ 
+ 
+ TFile* fin = TFile::Open("/Users/nicolasbize/Desktop/Stage_ALICE/AnalysisResults_cohJpsi_weighted.root");
+ //Dimuon Histo
+ TList* lCAnyDiMuonHist = (TList*) fin->Get("ReconstructedHistos_CAny");
+ lCAnyDiMuonHist->ls();
+
+ THnSparseT<TArrayD>* fHistoDiMuonOS = (THnSparseT<TArrayD>*) lCAnyDiMuonHist->FindObject("fHistoDiMuonOS");
+ 
+ fHistoDiMuonOS->GetAxis(0)->SetRange(fHistoDiMuonOS->GetAxis(0)->FindBin(1.9),fHistoDiMuonOS->GetAxis(0)->FindBin(2.6));
+ fHistoDiMuonOS->GetAxis(1)->SetRange(fHistoDiMuonOS->GetAxis(1)->FindBin(0.),fHistoDiMuonOS->GetAxis(1)->FindBin(1.));
+ 
+ Double_t par[6];
+ 
+ TF1* PowerLawfct = new TF1("PowerLawfct", PowerLaw, 0.,1.,4);
+ PowerLawfct->SetParameters(1.,1.,1.,1.);
+ PowerLawfct->SetLineStyle(4);
+ PowerLawfct->SetLineColor(kRed);
+ 
+ TF1* LogNormalfct = new TF1("LogNormalfct", LogNormal, 0.,1.,3);
+ LogNormalfct->SetParameters(1.,1.,1.);
+ LogNormalfct->SetLineStyle(4);
+ LogNormalfct->SetLineColor(kRed);
+
+ TF1* Gaussianfct = new TF1("Gaussianfct", Gaussian, 0.,1.,3);
+ Gaussianfct->SetParameters(1.,1.,1.);
+ Gaussianfct->SetLineStyle(4);
+ Gaussianfct->SetLineColor(kBlack);
+
+ TF1* Gaussianfct2 = new TF1("Gaussianfct2", Gaussian, 0.,1.,3);
+ Gaussianfct2->SetParameters(1.,1.,1.);
+ Gaussianfct2->SetLineStyle(4);
+ Gaussianfct2->SetLineColor(kGreen);
+
+ TF1* Tail = new TF1("Tail", TailExpo, 0., 1., 2);
+ Tail->SetParameters(1.,1.);
+ Tail->SetLineColor(kMagenta);
+ Tail->SetLineStyle(4);
+
+
+ TF1* GlobalFit = new TF1("GlobalFit", sum, 0.,1.,5);
+ GlobalFit->SetParameters(1.,1.,1. ,1.,1.);
+ //GlobalFit->SetParLimits(0,1.,500.);
+ //GlobalFit->SetParLimits(2,0.,10.);
+ //GlobalFit->SetParLimits(7,0.,1.e7);
+ //GlobalFit->SetParLimits(5,0.,2.);
+ //GlobalFit->SetParLimits(8,0.,2.);
+ //GlobalFit->SetParLimits(9,0.,1000.);
+ GlobalFit->SetLineColor(kBlue);
+
+
+ TH1D* h1 = fHistoDiMuonOS->Projection(1);
+ h1->SetTitle("Fit PowerLaw + Gaus + Gaus - Pt Distribution - mass range [1.9 - 2.6] - coh; p_{T} GeV/c; # DiMuons");
+ //h1->Fit("GlobalFit","R","");
+
+ fHistoDiMuonOS->GetAxis(0)->SetRange(fHistoDiMuonOS->GetAxis(0)->FindBin(2.6),fHistoDiMuonOS->GetAxis(0)->FindBin(3.4));
+ TH1D* h2 = fHistoDiMuonOS->Projection(1);
+ h2->SetTitle("Fit PowerLaw + Gaus + Gaus - Pt Distribution - mass range [2.6 - 3.4] - coh; p_{T} GeV/c; # DiMuons");
+ //GlobalFit->SetParameters(1.,1.,1.,1.,1.,1.,1.);
+ //h2->Fit("GlobalFit","R","");
+
+ fHistoDiMuonOS->GetAxis(0)->SetRange(fHistoDiMuonOS->GetAxis(0)->FindBin(3.4),fHistoDiMuonOS->GetAxis(0)->FindBin(5.));
+ TH1D* h3 = fHistoDiMuonOS->Projection(1);
+ h3->SetTitle("Fit Log Normal + TailExpo - Pt Distribution - mass range [3.4 - 5] - coh; p_{T} GeV/c; # DiMuons");
+ //GlobalFit->SetParameters(1.,1.,1.,1.,1.,1.,1.);
+ //h3->Fit("GlobalFit","R","");
+
+ fHistoDiMuonOS->GetAxis(0)->SetRange(fHistoDiMuonOS->GetAxis(0)->FindBin(1.9),fHistoDiMuonOS->GetAxis(0)->FindBin(5.));
+ TH1D* h4 = fHistoDiMuonOS->Projection(1);
+ h4->SetTitle("Fit PowerLaw + Gaus + Gaus - Pt Distribution - mass range [1.9 - 5] - coh; p_{T} GeV/c; # DiMuons");
+ //h4->Fit("GlobalFit","R","");
+ //GlobalFit->SetParameters(1.,1.,1.,1.,1.,1.,1.);
+ 
+
+ TCanvas* c1 = new TCanvas("FitLogNormalTailExpo_Pt Distribution_3.4_5._coh","mass fct pt coh");
+ c1->cd();
+ gPad->SetLogy();
+ gStyle->SetOptFit(1111);
+ h3->Draw("e");
+ h3->Fit("GlobalFit","EM","",0.,1.);
+ 
+ const char *formula = "f( p_{T} ) = #frac{p0}{2#pi p2 p_{T}} exp#left( - #frac{ln^{2}#left(p_{T} - p1#right)}{2 p2^{2}} #right)+ p3 p_{T} exp#left(- #frac{p_{T}}{p4}#right)";
+ TLatex latex;
+ latex.SetTextSize(0.025);
+ latex.DrawLatex(.1,100.,formula);
+ 
+ GlobalFit->GetParameters(par);
+ LogNormalfct->SetParameters(par);
+ //Gaussianfct->SetParameters(&par[3]);
+ Tail->SetParameters(&par[3]);
+ LogNormalfct->Draw("same");
+ //Gaussianfct->Draw("same");
+ Tail->Draw("same");
+
+}

87714191219075f28b483064e2639cd4c6d0e8b3
Nicolas Bizé <nicolasbize@services-mbp.home>
2021-04-28 16:22:57 +0200

    add new macro file for fit Pt distri on data with fixed normalization

--- fitPt_data.C
+++ fitPt_data.C
@@ -9,8 +9,6 @@
 #include "TLatex.h"
 #include "TF1.h"
 #include "TStyle.h"
-#include "TF1Convolution.h"
-//#include "FitFunctions.C"
 #include <cmath>
 #include <vector>
 #include <stdio.h>
@@ -28,7 +26,7 @@ Double_t LogNormal(Double_t *x, Double_t *par)
     //par[2]=width
     //par[0]=normalization
 
-    Double_t numExp =  (TMath::Log(x[0]) - par[1]) * (TMath::Log(x[0]) - par[1])  ;
+    Double_t numExp =  ( (TMath::Log(x[0])) - par[1] ) * ( (TMath::Log(x[0])) - par[1] )  ;
     Double_t denExp = 2 * par[2] * par[2];
     
     return par[0]/(x[0] * par[2] * TMath::Sqrt(2*TMath::Pi())) * TMath::Exp(- numExp / denExp) ;
@@ -84,63 +82,51 @@ void fitPt_data()
  Double_t par[14];
  for(int i=0; i<14;i++){Fitfct->SetParameters(i,1.);}
  Fitfct->SetLineColor(kRed);
+ Fitfct->SetLineWidth(4);
 
  //gamma gamma contribution
  fgamma->SetParameters(1.,1.,1.,1.,1.);
  fgamma->SetLineStyle(4);
+ fgamma->SetLineWidth(4);
  fgamma->SetLineColor(kBlue);
  
  //jpsi coh contribution
  fcohjpsi->SetParameters(1.,1.,1., 1.,1.);
  fcohjpsi->SetLineStyle(4);
+ fcohjpsi->SetLineWidth(4);
  fcohjpsi->SetLineColor(kMagenta);
 
  //jpsi incoh contribution
  fincohjpsi->SetParameters(1.,1.,1.);
  fincohjpsi->SetLineStyle(4);
- fincohjpsi->SetLineColor(kGreen);
+ fincohjpsi->SetLineWidth(4);
+ fincohjpsi->SetLineColor(kGreen+1);
 
  //hadro
  fhadro->SetParameters(0,1.);
  fhadro->SetLineStyle(4);
+ fhadro->SetLineWidth(4);
  fhadro->SetLineColor(kBlack);
 
 //Parameters for mass range [3.4-5.]
  
- //Fitfct->SetParLimits(0,1.,200.);
- Fitfct->SetParLimits(1,-4.,-1.); //fgamma p1
- Fitfct->SetParLimits(2,0.5,0.6); //fgamma p2
- Fitfct->SetParLimits(3,1.,1.e5); //fgamma p3
- Fitfct->SetParLimits(4,0.02,0.04);//fgamma p4
+ Fitfct->SetParLimits(0,1.,100.);
+ Fitfct->SetParLimits(1,-4.,-1.);  //fgamma p1
+ Fitfct->SetParLimits(2,0.4,1.);   //fgamma p2
+ Fitfct->SetParLimits(3,0.,300.);  //fgamma p3
+ Fitfct->FixParameter(4,0.1922);   //fgamma p4
 
-/*
- //Fitfct->SetParLimits(5,0.,50.); //fcoh p0 norm
- Fitfct->FixParameter(6,0.0003399);    //fcoh p1
- Fitfct->FixParameter(7,-1.372);     //fcoh p2
- Fitfct->FixParameter(8,0.05854);     //fcoh p3
- //Fitfct->SetParLimits(9,1.,50.);   //fcoh p4 norm
- Fitfct->FixParameter(10,0.2902);    //fcoh p5
- Fitfct->FixParameter(11,0.09176);   //fcoh p6
- //Fitfct->SetParLimits(12,1.,50.);   //fcoh p7 norm
- Fitfct->FixParameter(13,0.3382);    //fcoh p8
- Fitfct->FixParameter(14,0.2344);    //fcoh p9
-*/
-
- Fitfct->SetParLimits(5,0.,100.); //fcoh p0 norm
+ Fitfct->FixParameter(5,0.0475);    //fcoh p0 norm
  Fitfct->FixParameter(6,-0.7683);    //fcoh p1
- Fitfct->FixParameter(7,0.468);     //fcoh p2
- Fitfct->SetParLimits(8,0.,500.);     //fcoh p3 norm
- Fitfct->FixParameter(9,0.3596);   //fcoh p4
+ Fitfct->FixParameter(7,0.468);      //fcoh p2
+ Fitfct->FixParameter(8,0.21825);   //fcoh p3 norm
+ Fitfct->FixParameter(9,0.3596);     //fcoh p4
  
+ Fitfct->FixParameter(10,0.2524);   //fincoh p0 norm
+ Fitfct->FixParameter(11,-0.1312);   //fincoh p1
+ Fitfct->FixParameter(12,0.5448);    //fincoh p2
 
-
- Fitfct->SetParLimits(10,0.,60.);   //fincoh p0 norm
- Fitfct->FixParameter(11,-0.1312);    //fincoh p1
- Fitfct->FixParameter(12,0.5448);     //fincoh p2
-
- Fitfct->FixParameter(13,30.14);//fhadro p0
-
-
+ Fitfct->SetParLimits(13,30.71,32.65); //fhadro p0
 
  // file data
  TFile* fin = TFile::Open("/Users/nicolasbize/Desktop/Stage_ALICE/AnalysisResults_DataMerged.root");
@@ -149,37 +135,52 @@ void fitPt_data()
  lCMUL7DiMuonHist->ls();
 
  THnSparseT<TArrayD>* fHistoDiMuonOS = (THnSparseT<TArrayD>*) lCMUL7DiMuonHist->FindObject("fHistoDiMuonOS");
- fHistoDiMuonOS->GetAxis(0)->SetRange(fHistoDiMuonOS->GetAxis(0)->FindBin(1.9),fHistoDiMuonOS->GetAxis(0)->FindBin(2.6));
  fHistoDiMuonOS->GetAxis(1)->SetRange(fHistoDiMuonOS->GetAxis(1)->FindBin(0.),fHistoDiMuonOS->GetAxis(1)->FindBin(1.));
  fHistoDiMuonOS->GetAxis(3)->SetRange(fHistoDiMuonOS->GetAxis(3)->FindBin(70.), fHistoDiMuonOS->GetAxis(3)->FindBin(90.));
 
+
+ fHistoDiMuonOS->GetAxis(0)->SetRange(fHistoDiMuonOS->GetAxis(0)->FindBin(1.9),fHistoDiMuonOS->GetAxis(0)->FindBin(2.6));
  TH1D* h1 = fHistoDiMuonOS->Projection(1);
  h1->SetTitle("Fit - Pt Distribution - mass range [1.9 - 2.6] - data; p_{T} GeV/c; # DiMuons");
- //h1->Fit("Fitfct","R","");
+ h1->SetLineColor(kBlack);
+ h1->SetMarkerStyle(20);
+ h1->SetMarkerSize(0.4);
 
  fHistoDiMuonOS->GetAxis(0)->SetRange(fHistoDiMuonOS->GetAxis(0)->FindBin(2.6),fHistoDiMuonOS->GetAxis(0)->FindBin(3.4));
  TH1D* h2 = fHistoDiMuonOS->Projection(1);
  h2->SetTitle("Fit - Pt Distribution - mass range [2.6 - 3.4] - data; p_{T} GeV/c; # DiMuons");
- //h2->Fit("Fitfct","R","");
+ h2->SetLineColor(kBlack);
+ h2->SetMarkerStyle(20);
+ h2->SetMarkerSize(0.4);
 
  fHistoDiMuonOS->GetAxis(0)->SetRange(fHistoDiMuonOS->GetAxis(0)->FindBin(3.4),fHistoDiMuonOS->GetAxis(0)->FindBin(5.));
  TH1D* h3 = fHistoDiMuonOS->Projection(1);
  h3->SetTitle("Fit - Pt Distribution - mass range [3.4 - 5] - data; p_{T} GeV/c; # DiMuons");
- //h3->Fit("Fitfct","R","");
+ h3->SetLineColor(kBlack);
+ h3->SetMarkerStyle(20);
+ h3->SetMarkerSize(0.4);
 
  fHistoDiMuonOS->GetAxis(0)->SetRange(fHistoDiMuonOS->GetAxis(0)->FindBin(1.9),fHistoDiMuonOS->GetAxis(0)->FindBin(5.));
  TH1D* h4 = fHistoDiMuonOS->Projection(1);
  h4->SetTitle("Fit - Pt Distribution - mass range [1.9 - 5] - data; p_{T} GeV/c; # DiMuons");
- //h4->Fit("Fitfct","R","");
- 
+ h4->SetLineColor(kBlack);
+ h4->SetMarkerStyle(20);
+ h4->SetMarkerSize(0.4);
+
 
 
  TCanvas* c1 = new TCanvas("PtDistribution data","mass fct pt data");
- c1->cd();
- gPad->SetLogy();
+
+ const char *formula = "f(p_{T}) = p0 p_{T}";
+ TLatex latex;
+ latex.SetTextSize(0.025); gStyle->SetOptFit(1111);
+ latex.DrawLatex(.2,10.,formula);
+
+ c1->cd(); gPad->SetLogy();
  gStyle->SetOptFit(1111);
+
  h3->Fit("Fitfct","EM","",0.,1.);
- h3->Draw("e");
+ h3->Draw("esame");
  
  Fitfct->GetParameters(par);
  fgamma->SetParameters(par);
@@ -191,6 +192,16 @@ void fitPt_data()
  fcohjpsi->Draw("same");
  fincohjpsi->Draw("same");
  fhadro->Draw("same");
- c1->BuildLegend(0.67,0.14,0.90,0.28);
+ c1->BuildLegend(0.23,0.68,0.6,0.88);
+ fHistoDiMuonOS->GetAxis(0)->SetRange(fHistoDiMuonOS->GetAxis(0)->FindBin(3.4),fHistoDiMuonOS->GetAxis(0)->FindBin(5.));
+
+ Double_t N_JPsiCoh = fcohjpsi->Integral(0,0.3)/0.05;
+ cout << "N_JPsiCoh= " << N_JPsiCoh << endl;
+ Double_t N_JPsiIncoh = fincohjpsi->Integral(0,0.3)/0.05;
+ cout << "N_JPsiIncoh= " << N_JPsiIncoh << endl;
+ Double_t N_GG = fgamma->Integral(0,0.3)/0.05;
+ cout << "N_GG= " << N_GG << endl;
+ Double_t N_hadro = fhadro->Integral(0,0.3)/0.05;
+ cout << "N_hadro= " << N_hadro << endl;
 
 }

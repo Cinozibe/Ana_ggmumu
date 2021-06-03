ca61aadf0a51c01170a68f2744dd62fe3e976f19
Nicolas Bizé <nicolasbize@nanpc418.in2p3.fr>
2021-06-03 15:47:39 +0200

    add new version of the macro on the 2-dimensional fit of the gg to mumu analysis

--- plot_lego.C
+++ plot_lego.C
@@ -123,13 +123,15 @@ Double_t poly6(Double_t *y, Double_t *par)
 
 Double_t FitFunctionMass(Double_t *y, Double_t *par)
 {
-	return  poly6(y, par) + CrystalBallExtended(y,&par[7]) + CrystalBallExtended(y,&par[14]); //+ CrystalBallExtended(y,&par[21]) HADRO JPSI
+	return  poly6(y, par) + CrystalBallExtended(y,&par[7]) + CrystalBallExtended(y,&par[14]) + CrystalBallExtended(y,&par[21]);// HADRO JPSI
 }
 
 Double_t FitFunctionTotal(Double_t *x,  Double_t *par)
 {
     return  FitFunctionPt(x,par) + FitFunctionMass(&x[1],&par[8]);
 }
+
+
 
 void plot_lego(){
 
@@ -156,14 +158,13 @@ void plot_lego(){
  gHisto_incoh->SetDirectory(0); // This is needed because otherwise gHisto belongs to TFile and it is deleted when TFIle is closed
  delete file_incoh;
 
-
- 
-
+//fit function created with 36 parameters
  TF2* Fitfct = new TF2("Fitfct",FitFunctionTotal,0.,1.,1.9,5.,36);
  Double_t par[36];
  for(int i=0;i<36;i++){Fitfct->SetParameters(i,1.);}
- //contribution JPsi
- //CBExtended
+
+
+//CB Extended parameters for the 3 different jpsi contributions (coh, incoh, hadro)
  Double_t AlphaL_coh = 0.8096;
  Double_t nL_coh = 135.747;
  Double_t AlphaR_coh= 2.3248;
@@ -179,48 +180,31 @@ void plot_lego(){
  Double_t AlphaR_hadro = 3.043;
  Double_t nR_hadro = 15.323;
 
-  //Parameters for mass range [1.9-5]
+//Parameters fixed for mass range [1.9-5] GeV/c2 and pt [0-1] GeV/c
 
- Fitfct->FixParameter(0,47.53);
- Fitfct->FixParameter(1,-2.167);  //fgamma p1
- Fitfct->FixParameter(2,0.7125);   //fgamma p2
-/*
- Fitfct->SetParLimits(0,1.,100.);
- Fitfct->SetParLimits(1,-4.,-1.);  //fgamma p1
- Fitfct->SetParLimits(2,0.4,1.);   //fgamma p2
- */
- Fitfct->SetParLimits(3,0.,300.);  //fgamma p3
- Fitfct->FixParameter(4,0.1846);   //fgamma p4
+ Fitfct->FixParameter(0,47.53);       //fgamma p0
+ Fitfct->FixParameter(1,-2.167);      //fgamma p1
+ Fitfct->FixParameter(2,0.7125);      //fgamma p2
+ Fitfct->FixParameter(3,2.365e-6);    //fgamma p3
+ Fitfct->FixParameter(4,0.1846);      //fgamma p4
 
- Fitfct->FixParameter(5,0.00059688);    //fcoh p0 norm
+ Fitfct->FixParameter(5,0.00059688);  //fcoh p0 norm template
  
- Fitfct->FixParameter(6,0.000319);   //fincoh p0 norm
+ Fitfct->FixParameter(6,0.000319);    //fincoh p0 norm template
 
-//fhadro p0
- Fitfct->SetParLimits(7,569.3,589.7);
-// Fitfct->SetParLimits(7,50.,589.7);
+ Fitfct->FixParameter(7,569.3);       //fhadro p0
 
- Fitfct->FixParameter(8,-130.8); //gg contribution
- Fitfct->FixParameter(9,127.5);
- Fitfct->FixParameter(10,-22.57);
- Fitfct->FixParameter(11,-4.394);
- Fitfct->FixParameter(12,0.5879);
- Fitfct->FixParameter(13,0.2604);
- Fitfct->FixParameter(14,-0.03669);
-
-/*
- Fitfct->SetParameters(8,1.043e4);     //gg contribution
- Fitfct->SetParameters(9,-1.92e4);
- Fitfct->SetParameters(10,1.292e4);
- Fitfct->SetParameters(11,-3.509e3);
- Fitfct->SetParameters(12,85.05);
- Fitfct->SetParameters(13,150.9);
- Fitfct->SetParameters(14,-29.43);
- Fitfct->SetParameters(15,1.745);
-*/
+ //gg contribution
+ Fitfct->FixParameter(8,-191.5); 
+ Fitfct->FixParameter(9,262.3);
+ Fitfct->FixParameter(10,-48.64);
+ Fitfct->FixParameter(11,-12.86);
+ Fitfct->FixParameter(12,1.643);
+ Fitfct->FixParameter(13,0.7639);
+ Fitfct->FixParameter(14,-0.1094);
 
  //coherent jpsi contribution
- Fitfct->FixParameter(15,171.4);
+ Fitfct->FixParameter(15,174.595);
  Fitfct->FixParameter(16,3.108);
  Fitfct->FixParameter(17,0.05666);
  Fitfct->FixParameter(18,AlphaL_coh);
@@ -228,8 +212,8 @@ void plot_lego(){
  Fitfct->FixParameter(20,AlphaR_coh);
  Fitfct->FixParameter(21,nR_coh);
 
- //Fitfct->FixParameter(22,2.661);       //incoherent jpsi contribution
- Fitfct->FixParameter(22,26.61);
+ //incoherent jpsi contribution
+ Fitfct->FixParameter(22,85.08);
  Fitfct->FixParameter(23,3.108);
  Fitfct->FixParameter(24,-0.05647);
  Fitfct->FixParameter(25,AlphaL_incoh);
@@ -237,7 +221,8 @@ void plot_lego(){
  Fitfct->FixParameter(27,AlphaR_incoh);
  Fitfct->FixParameter(28,nR_incoh);
 
- Fitfct->SetParLimits(29,0.1,10.);    //hadronic jpsi contribution
+ //hadronic jpsi contribution
+ Fitfct->FixParameter(29,114.7);
  Fitfct->FixParameter(30,3.108);
  Fitfct->FixParameter(31,-0.05647);
  Fitfct->FixParameter(32,AlphaL_hadro);
@@ -250,7 +235,6 @@ void plot_lego(){
  TFile* fin = TFile::Open("/Users/nicolasbize/Desktop/Stage_ALICE/analysisfiles/AnalysisResults_DataMerged.root");
  
  TList* lCMUL7DiMuonHist = (TList*) fin->Get("DiMuonHistos_CMUL7");
- //lCMUL7DiMuonHist->ls();
 
  THnSparseT<TArrayD>* fHistoDiMuonOS = (THnSparseT<TArrayD>*) lCMUL7DiMuonHist->FindObject("fHistoDiMuonOS");
  fHistoDiMuonOS->GetAxis(0)->SetRange(fHistoDiMuonOS->GetAxis(0)->FindBin(1.9),fHistoDiMuonOS->GetAxis(0)->FindBin(5.));
@@ -261,12 +245,12 @@ void plot_lego(){
  TH2D* h1 = fHistoDiMuonOS->Projection(0,1);
  h1->SetTitle("p_{T} M_{#mu#mu}; p_{T} GeV/c; M_{#mu#mu} GeV/c^{2}; entries");
 
- TH1D* hData = (TH1D*) fHistoDiMuonOS->Projection(1);
+ TH1D* hData = (TH1D*) fHistoDiMuonOS->Projection(1); //histo 1D for pt distri
  hData->SetLineColor(kBlue+1);
  hData->SetMarkerStyle(20);
  hData->SetMarkerSize(0.4);
 
- TH1D* hData_mass = (TH1D*) fHistoDiMuonOS->Projection(0);
+ TH1D* hData_mass = (TH1D*) fHistoDiMuonOS->Projection(0); //histo 1D for mass distri
  hData_mass->SetLineColor(kBlue+1);
  hData_mass->SetMarkerStyle(20);
  hData_mass->SetMarkerSize(0.4);
@@ -275,39 +259,7 @@ void plot_lego(){
  c1->cd();
  gStyle->SetOptFit(1111);
  h1->Fit("Fitfct","REM","");
-
- TF1* CBE_coh = new TF1("CBE_coh",CrystalBallExtended,1.9,5.,7);
- CBE_coh->SetParameters(1.,1.,1.,1.,1.,1.,1.);
- CBE_coh->SetLineStyle(4);
- CBE_coh->SetLineWidth(4);
- CBE_coh->SetLineColor(kMagenta);
-
- TF1* CBE_incoh = new TF1("CBE_incoh",CrystalBallExtended,1.9,5.,7);
- CBE_incoh->SetParameters(10.,1.,1.,1.,1.,1.,1.);
- CBE_incoh->SetLineStyle(4);
- CBE_incoh->SetLineWidth(4);
- CBE_incoh->SetLineColor(kGreen+1);
-
- TF1* CBE_hadro = new TF1("CBE_hadro",CrystalBallExtended,1.9,5.,7);
- CBE_hadro->SetParameters(1.,1.,1.,1.,1.,1.,1.);
- CBE_hadro->SetLineStyle(4);
- CBE_hadro->SetLineWidth(4);
- CBE_hadro->SetLineColor(kYellow+1);
-
-  //contribution gamma gamma
- TF1* fgamma = new TF1("fgamma", poly6, 1.9,5.,7);
- fgamma->SetParameters(1.,1.,1.,1.,1.,1.,1.);
- fgamma->SetLineStyle(4);
- fgamma->SetLineWidth(4);
- fgamma->SetLineColor(kBlue);
-
- fgamma->GetParameters(&par[8]);
- CBE_coh->GetParameters(&par[15]);
- CBE_incoh->GetParameters(&par[22]);
- CBE_hadro->GetParameters(&par[29]);
-
  h1->Draw("lego2");
-
 
 //draw the pt and mass distribution
  TCanvas* c2 = new TCanvas("c2","proj pt");
@@ -317,6 +269,7 @@ void plot_lego(){
  TH1D* hProjPt = h1->ProjectionX("Pt_Distri",0,-1,"");
  hProjPt->SetTitle("p_{T} Distribution");
  hProjPt->Draw("e");
+
  c2->cd(2);
 
  TH1D* hProjMass = h1->ProjectionY("Mass_Distri",0,-1,"");
@@ -326,7 +279,8 @@ void plot_lego(){
 
 // comparison between data and pt distribution 3D fit
  TH2D* hRandFit_Pt = new TH2D("hRandFit_Pt", "histo filled with fitfct; p_{T} GeV/c ;entries", 21, 0., 1., 156, 1.9, 5.);
- hRandFit_Pt->FillRandom("Fitfct",8777);
+ hRandFit_Pt->FillRandom("Fitfct",8777000);
+ hRandFit_Pt->Scale(0.001);
 
  TH1D* h2 = hRandFit_Pt->ProjectionX();
  h2->SetLineColor(kRed);
@@ -338,7 +292,7 @@ void plot_lego(){
  TCanvas* c3 = new TCanvas("c3", "test rand fit");
  c3->cd();
  h2->Draw("e");
- h2->GetYaxis()->SetRangeUser(0,1000);
+ h2->GetYaxis()->SetRangeUser(150.,1000);
  hData->Draw("esame");
  c3->BuildLegend(0.30,0.60,0.65,0.75);
  h2->SetTitle("Comparaison data V 2D fit function");
@@ -346,23 +300,32 @@ void plot_lego(){
 
 //comparison between data and mass distribution 3D fit
  TH2D* hRandFit_Mass = new TH2D("hRandFit_Mass", "histo filled with fitfct; p_{T} GeV/c ;M_{#mu#mu} GeV/c^{2}", 21, 0., 1., 156, 1.9, 5.);
- hRandFit_Mass->FillRandom("Fitfct",8777);
+ hRandFit_Mass->FillRandom("Fitfct",8777000);
+
+ //scale to have a good comparison between data and fit
+ hRandFit_Mass->Scale(0.001);
 
  TH1D* h3 = hRandFit_Mass->ProjectionY();
  h3->SetLineColor(kRed);
  h3->SetMarkerStyle(4);
  h3->SetMarkerSize(0.4);
 
- hData_mass->SetTitle("Data mass distribution");
-
  TCanvas* c4 = new TCanvas("c4", "test rand fit mass");
  c4->cd();
  h3->Draw("e");
- h3->GetYaxis()->SetRangeUser(0,1000);
+ h3->GetYaxis()->SetRangeUser(0.001,500.);
+ hData_mass->SetTitle("Data mass distribution");
  hData_mass->Draw("esame");
  
  c4->BuildLegend(0.30,0.60,0.65,0.75);
  h3->SetTitle("Comparaison data V 2D fit function");
+ 
+ TCanvas* c5 = new TCanvas("c5", "test 2 windows",1300.,500.);
+ c5->Divide(2,1);
+ c5->cd(1);
+ //h3->SetTitle("histo filled with fit function (rescaled)");
+ h3->Draw("e");
 
-
+ c5->cd(2);
+ hData_mass->Draw("e");
 }
